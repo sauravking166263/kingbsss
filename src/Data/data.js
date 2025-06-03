9234f1b7613ee5b90ea7513be34aafc8a17555e4
sauravking166263 <sooo91955@gmail.com>
2025-06-04 00:33:31 +0530

    1

---
+++ src/Data/data.js
@@ -0,0 +1,233 @@
+
+
+export const data = [
+    {
+        id: 1,
+        type: 'product',
+        title: "BREAST & VAGINA HOT SEXY DOLL FOR MEN MASTURBATION ENJOY REAL PARTNER SEX WITH FREE LUBE(LOW PRICE SEX TOY)",
+        image: "https://g.sdlcdn.com/imgs/k/y/j/BREAST-VAGINA-HOT-SEXY-DOLL-SDL512728765-1-42ba1.jpg",
+        discount: "87% Off",
+        price: "₹1846",
+        additionalPrice: "₹189.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/y/j/BREAST-VAGINA-HOT-SEXY-DOLL-SDL512728765-1-42ba1.jpg",
+            "https://g.sdlcdn.com/imgs/k/y/j/BREAST-VAGINA-HOT-SEXY-DOLL-SDL512728765-2-14236.jpg",
+            "https://g.sdlcdn.com/imgs/k/y/j/BREAST-VAGINA-HOT-SEXY-DOLL-SDL512728765-3-7d60b.jpg",
+            "https://g.sdlcdn.com/imgs/k/y/j/BREAST-VAGINA-HOT-SEXY-DOLL-SDL512728765-4-e371c.jpg",
+            "https://g.sdlcdn.com/imgs/k/y/j/BREAST-VAGINA-HOT-SEXY-DOLL-SDL512728765-5-6585b.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/downloadrv_bv8rSZE.jpg"
+    },
+    {
+        id: 2,
+        type: 'product',
+        title: "KAMAHOUSE REALSTIC MASTURBATOR MY GURL POCKET PUSSY SEX TOY FOR MEN (LOW PRICE SEXY DOLL)",
+        image: "https://g.sdlcdn.com/imgs/k/z/p/KAMAHOUSE-REALSTIC-MASTURBATOR-MY-GURL-SDL851220619-1-cde9c.jpg",
+        discount: "90   % Off",
+        price: "₹1999",
+        additionalPrice: "₹239.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/z/p/KAMAHOUSE-REALSTIC-MASTURBATOR-MY-GURL-SDL851220619-1-cde9c.jpg",
+            "https://g.sdlcdn.com/imgs/k/z/p/KAMAHOUSE-REALSTIC-MASTURBATOR-MY-GURL-SDL851220619-2-49754.jpg",
+            "https://g.sdlcdn.com/imgs/k/z/p/KAMAHOUSE-REALSTIC-MASTURBATOR-MY-GURL-SDL851220619-4-bff43.jpg",
+            "https://g.sdlcdn.com/imgs/k/z/p/KAMAHOUSE-REALSTIC-MASTURBATOR-MY-GURL-SDL851220619-8-77e50.jpg",
+            "https://g.sdlcdn.com/imgs/k/z/p/KAMAHOUSE-REALSTIC-MASTURBATOR-MY-GURL-SDL851220619-10-d8d6c.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/rv.jpg"
+    },
+    {
+        id: 3,
+        type: 'product',
+        title: "LIKA MALE POCKET PUSSY MASTURBATOR Masturbator CUP",
+        image: "https://g.sdlcdn.com/imgs/k/s/d/KAMAHOUSE-SASHA-MALIKA-MALE-POCKET-SDL316648040-1-9ed8e.jpg",
+        discount: "78% Off",
+        price: "1899",
+        additionalPrice: "219.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/s/d/KAMAHOUSE-SASHA-MALIKA-MALE-POCKET-SDL316648040-1-9ed8e.jpg",
+            "https://g.sdlcdn.com/imgs/k/s/d/KAMAHOUSE-SASHA-MALIKA-MALE-POCKET-SDL316648040-2-b5e4f.jpg",
+            "https://g.sdlcdn.com/imgs/k/s/d/KAMAHOUSE-SASHA-MALIKA-MALE-POCKET-SDL316648040-4-1e478.jpg",
+            "https://g.sdlcdn.com/imgs/k/s/d/KAMAHOUSE-SASHA-MALIKA-MALE-POCKET-SDL316648040-3-1c76b.jpg",
+            "https://g.sdlcdn.com/imgs/k/s/d/KAMAHOUSE-SASHA-MALIKA-MALE-POCKET-SDL316648040-5-0aa7e.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/singham.jpg"
+    },
+    {
+        id: 4,
+        type: 'product',
+        title: "7.5  INCH POCKET PUSSY TIGHT VAGINA MASTURBATION CUP FOR MALE BY KAMAHOUSE",
+        image: "https://g.sdlcdn.com/imgs/k/3/6/NAUGHTY-TOYS-PRESENT-7-5-SDL463741433-1-78c97.jpg",
+        discount: "76% Off",
+        price: "₹1990",
+        additionalPrice: "215.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/3/6/NAUGHTY-TOYS-PRESENT-7-5-SDL463741433-1-78c97.jpg",
+            "https://g.sdlcdn.com/imgs/k/3/6/NAUGHTY-TOYS-PRESENT-7-5-SDL463741433-2-124c6.jpg",
+            "https://g.sdlcdn.com/imgs/k/3/6/NAUGHTY-TOYS-PRESENT-7-5-SDL463741433-3-672d9.jpg",
+            "https://g.sdlcdn.com/imgs/k/3/6/NAUGHTY-TOYS-PRESENT-7-5-SDL463741433-4-13a27.jpg",
+            "https://g.sdlcdn.com/imgs/k/3/6/NAUGHTY-TOYS-PRESENT-7-5-SDL463741433-5-021ee.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/download_rMYhJP3.jpg"
+    },
+    {
+        id: 5,
+        type: 'product',
+        title: "Pocket Pussy with Real Mouth and Tongue Masturbator Low Price Sex Toy For Men",
+        image: "https://g.sdlcdn.com/imgs/k/t/g/KAMAHOUSE-Pocket-Pussy-with-Real-SDL274846594-1-d5b7e.jpg",
+        discount: "76% Off",
+        price: "₹999",
+        additionalPrice: "189.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/t/g/KAMAHOUSE-Pocket-Pussy-with-Real-SDL274846594-1-d5b7e.jpg",
+            "https://g.sdlcdn.com/imgs/k/t/g/KAMAHOUSE-Pocket-Pussy-with-Real-SDL274846594-2-dca5c.jpg",
+            "https://g.sdlcdn.com/imgs/k/t/g/KAMAHOUSE-Pocket-Pussy-with-Real-SDL274846594-3-ef384.jpg",
+            "https://g.sdlcdn.com/imgs/k/t/g/KAMAHOUSE-Pocket-Pussy-with-Real-SDL274846594-5-20628.png  "
+            
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/download_MSKov80.jpg"
+    },
+    {
+        id: 6,
+        type: 'product',
+        title: "Easy to Hide Male Stroker Body Safe Silicone Material Easy to Wash Penis Masturbator For Men",
+        image: "https://g.sdlcdn.com/imgs/k/4/f/Secret-Masturbator-Easy-to-Hide-SDL196331053-1-924eb.jpg",
+        discount: "87% Off",
+        price: "₹1999",
+        additionalPrice: "₹199.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/4/f/Secret-Masturbator-Easy-to-Hide-SDL196331053-1-924eb.jpg",
+            "https://g.sdlcdn.com/imgs/k/4/f/Secret-Masturbator-Easy-to-Hide-SDL196331053-2-7fc4c.jpg",
+            "https://g.sdlcdn.com/imgs/k/4/f/Secret-Masturbator-Easy-to-Hide-SDL196331053-3-b0476.jpg",
+            "https://g.sdlcdn.com/imgs/k/4/f/Secret-Masturbator-Easy-to-Hide-SDL196331053-4-1062e.jpg",
+
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/download_Xj2qTgj.jpg"
+    },
+    {
+        id: 7,
+        type: 'product',
+        title: "SOLO HAND FREE LOTUS CUP MASTURBATOR SOFT&REAL PUSSY FOR MEN",
+        image: "https://g.sdlcdn.com/imgs/k/2/q/KAMAHOUSE-SOLO-HAND-FREE-LOTUS-SDL150482498-1-ceaec.jpg",
+        discount: "85% Off",
+        price: "₹2999",
+        additionalPrice: "₹379.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/2/q/KAMAHOUSE-SOLO-HAND-FREE-LOTUS-SDL150482498-1-ceaec.jpg",
+            "https://g.sdlcdn.com/imgs/k/2/q/KAMAHOUSE-SOLO-HAND-FREE-LOTUS-SDL150482498-2-5d28f.png",
+            "https://g.sdlcdn.com/imgs/k/2/q/KAMAHOUSE-SOLO-HAND-FREE-LOTUS-SDL150482498-5-584fe.jpg",
+            "https://g.sdlcdn.com/imgs/k/2/q/KAMAHOUSE-SOLO-HAND-FREE-LOTUS-SDL150482498-5-584fe.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/comimg_mdobJnO.jpg"
+    },
+    {
+        id: 8,
+        type: 'product',
+        title: "QING JUMBO PUSSY (IE-NOA) CUP POCKET PUSSY FOR MALE (MULTI COLOR)",
+        image: "https://g.sdlcdn.com/imgs/k/z/z/NAUGHTY-TOYS-PRESENT-QING-JUMBO-SDL185763994-8-88d94.jpg",
+        discount: "89% Off",
+        price: "₹1999",
+        additionalPrice: "₹235.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/z/z/NAUGHTY-TOYS-PRESENT-QING-JUMBO-SDL185763994-8-88d94.jpg",
+            "https://g.sdlcdn.com/imgs/k/z/z/NAUGHTY-TOYS-PRESENT-QING-JUMBO-SDL185763994-7-45075.jpg",
+            "https://g.sdlcdn.com/imgs/k/z/z/NAUGHTY-TOYS-PRESENT-QING-JUMBO-SDL185763994-3-90491.jpg",
+            "https://g.sdlcdn.com/imgs/k/z/z/NAUGHTY-TOYS-PRESENT-QING-JUMBO-SDL185763994-2-53422.jpg"
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/sdfgsdfgsdfgsdfg.jpg"
+    },
+    {
+        id: 9,
+        type: 'product',
+        title: "Pocket Pussy Masturbator with Multi Speed Vibration for Ultra Climax",
+        image: "https://g.sdlcdn.com/imgs/k/3/b/Pocket-Pussy-Masturbator-with-Multi-SDL673968578-1-85e06.jpg",
+        discount: "76% Off",
+        price: "₹2799",
+        additionalPrice: "309.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/3/b/Pocket-Pussy-Masturbator-with-Multi-SDL673968578-1-85e06.jpg",
+            "https://g.sdlcdn.com/imgs/k/3/b/Pocket-Pussy-Masturbator-with-Multi-SDL673968578-2-81e80.jpg",
+            "https://g.sdlcdn.com/imgs/k/3/b/Pocket-Pussy-Masturbator-with-Multi-SDL673968578-3-393a3.jpg",
+            "https://g.sdlcdn.com/imgs/k/3/b/Pocket-Pussy-Masturbator-with-Multi-SDL673968578-4-1d1d8.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/egergwertgqertgwertg.jpg"
+    },
+    {
+        id: 10,
+        type: 'product',
+        title: "Metal Diamond Butt Plug Anall Plug Bead Pro state Massage Sex Toys for Women/ Men",
+        image: "https://g.sdlcdn.com/imgs/k/z/v/Metal-Diamond-Butt-Plug-Anall-SDL442183815-1-ca518.png",
+        discount: "82% Off",
+        price: "₹770",
+        additionalPrice: "₹119.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/z/v/Metal-Diamond-Butt-Plug-Anall-SDL442183815-1-ca518.png",
+            "https://g.sdlcdn.com/imgs/j/u/n/Metal-Diamond-Butt-Plug-Anall-SDL442183815-1-ac9b7.jpg",
+            "https://g.sdlcdn.com/imgs/j/u/n/Metal-Diamond-Butt-Plug-Anall-SDL442183815-3-dee9c.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/asoldjfhaosdfoasdjfkl.jpg"
+    },
+    {
+        id: 11,
+        type: 'product',
+        title: "Handjob Male Masturbator Sex Toy for Men Silicone Vagina Masturbation Cup Pocket Pussy",
+        image: "https://g.sdlcdn.com/imgs/k/6/a/Handjob-Male-Masturbator-Sex-Toy-SDL173078184-1-eef13.jpg",
+        discount: "80% Off",
+        price: "₹1899",
+        additionalPrice: "259.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/6/a/Handjob-Male-Masturbator-Sex-Toy-SDL173078184-1-eef13.jpg",
+            "https://g.sdlcdn.com/imgs/k/6/a/Handjob-Male-Masturbator-Sex-Toy-SDL173078184-2-9f2e4.jpg",
+            "https://g.sdlcdn.com/imgs/k/6/a/Handjob-Male-Masturbator-Sex-Toy-SDL173078184-3-befbd.jpg",
+            "https://g.sdlcdn.com/imgs/k/6/a/Handjob-Male-Masturbator-Sex-Toy-SDL173078184-7-99c93.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/asdfgwerfghweqrtgyqertqertqwert.jpg"
+    },
+    {
+        id: 12,
+        type: 'product',
+        title: "2 In 1 Mouth Vagina 3D Pocket Pussy Blow Job Indian Dark Male Masturbator Sex Toy For Men",
+        image: "https://g.sdlcdn.com/imgs/k/2/p/2-In-1-Mouth-Vagina-SDL894931949-1-06f48.jpg",
+        discount: "90% Off",
+        price: "₹2699",
+        additionalPrice: "₹289.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/2/p/2-In-1-Mouth-Vagina-SDL894931949-1-06f48.jpg",
+            "https://g.sdlcdn.com/imgs/k/2/p/2-In-1-Mouth-Vagina-SDL894931949-2-e063d.jpg",
+            "https://g.sdlcdn.com/imgs/k/2/p/2-In-1-Mouth-Vagina-SDL894931949-3-77358.jpg",
+            "https://g.sdlcdn.com/imgs/k/2/p/2-In-1-Mouth-Vagina-SDL894931949-4-c2ee9.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/jgtyujrtyhj.jpg"
+    },
+    {
+        id: 13,
+        type: 'product',
+        title: "Masturbator Pocket Pussy Sex Toy Vagina Pussy",
+        image: "https://g.sdlcdn.com/imgs/k/x/y/qing-Masturbator-Pocket-Pussy-Sex-SDL505892147-1-67a44.jpg",
+        discount: "75% Off",
+        price: "₹1299",
+        additionalPrice: "₹249.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/x/y/qing-Masturbator-Pocket-Pussy-Sex-SDL505892147-1-67a44.jpg",
+            "https://g.sdlcdn.com/imgs/k/x/y/qing-Masturbator-Pocket-Pussy-Sex-SDL505892147-2-9b4b8.jpg",
+            "https://g.sdlcdn.com/imgs/k/e/j/Indian-Beauty-Masturbator-Pocket-Pussy-SDL505892147-2-191a1.jpg",
+            "https://g.sdlcdn.com/imgs/k/e/j/Indian-Beauty-Masturbator-Pocket-Pussy-SDL505892147-3-b310c.jpg"
+
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/fasdfasdfasdf.jpg"
+    },
+    {
+        id: 14,
+        type: 'product',
+        title: "Full Body Silicone Pocket Pussy Sex Doll With Breast And pussy For Masturbation Toy",
+        image: "https://g.sdlcdn.com/imgs/k/m/9/SEX-TANTRA-Half-Body-Silicone-SDL705395077-1-34dd8.jpg",
+        discount: "90% Off",
+        price: "₹3699",
+        additionalPrice: "₹649.00",
+        sliderImages: [
+            "https://g.sdlcdn.com/imgs/k/m/9/SEX-TANTRA-Half-Body-Silicone-SDL705395077-1-34dd8.jpg",
+            "https://g.sdlcdn.com/imgs/k/p/k/SEX-TANTRA-Half-Body-Silicone-SDL705395077-1-1e119.jpg",
+            "https://g.sdlcdn.com/imgs/k/p/k/SEX-TANTRA-Half-Body-Silicone-SDL705395077-3-e988a.jpg",
+            "https://g.sdlcdn.com/imgs/k/p/k/SEX-TANTRA-Half-Body-Silicone-SDL705395077-4-b4e35.jpg",
+            "https://g.sdlcdn.com/imgs/k/p/k/SEX-TANTRA-Half-Body-Silicone-SDL705395077-5-a4c6e.jpg"
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/asdfgawedtgqerg.jpg"
+    },
+    
+];
